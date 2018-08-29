a6538963b23fb62cdfd029f9c6aacd5e130f4d85
pierrepoulouin <pierrepoulouin@gmail.com>
2018-08-29 12:13:07 +0200

    Ajout des infos

---
+++ ArrayListBDD/ressources/InsererInfoPersonne.sql
@@ -0,0 +1,11 @@
+INSERT INTO `users`.`personne`
+(`nom`,
+`prenom`,
+`age`,
+`email`,
+`telephone`)
+VALUES
+('Coin','Coin',30,'coincoin@duck.coin', '0606060606'),
+('Lucien','Maurice',45,'heyhodubato@coucou', '0707070707'),
+('Django', 'Hudada',21,'louxor@god.dom', '0808080808'),
+('Jonasz', 'Lefort', 12, 'jmMinecraftlol@kikoo.fr', '0909090909')
